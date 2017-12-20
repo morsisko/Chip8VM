ea2e85d921e313e343bbb796e497fe4a37ccc4e7
morsisko <morsisko@gmail.com>
2017-12-20 20:11:43 +0100

    initialization of helpers

--- Chip8VM/Chip8.cpp
+++ Chip8VM/Chip8.cpp
@@ -1,8 +1,8 @@
 #include "Chip8.h"
 
+//Resource used: http://devernay.free.fr/hacks/chip8/C8TECH10.HTM
 
-
-Chip8::Chip8() : 
+Chip8::Chip8() :
 	V({}),
 	stack({}),
 	memory({}),
@@ -13,6 +13,30 @@ Chip8::Chip8() :
 {
 }
 
+ushort Chip8::GetNNN(ushort opcode)
+{
+	return opcode & 0x0FFF;
+}
+
+uchar Chip8::GetN(ushort opcode)
+{
+	return opcode & 0x000F;
+}
+
+uchar Chip8::GetX(ushort opcode)
+{
+	return (opcode & 0x0F00) >> 8;
+}
+
+uchar Chip8::GetY(ushort opcode)
+{
+	return (opcode & 0x00F0) >> 4;
+}
+
+uchar Chip8::GetKK(ushort opcode)
+{
+	return opcode & 0x00FF;
+}
 
 Chip8::~Chip8()
 {
