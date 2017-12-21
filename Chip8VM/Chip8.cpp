a2d547af2ec608a9a696c37b76c72923e140ba73
morsisko <morsisko@gmail.com>
2017-12-21 23:29:10 +0100

    add base opcodes

--- Chip8VM/Chip8.cpp
+++ Chip8VM/Chip8.cpp
@@ -38,6 +38,84 @@ uchar Chip8::GetKK(ushort opcode)
 	return opcode & 0x00FF;
 }
 
+void Chip8::Do1NNN(ushort opcode)
+{
+	pc = GetNNN(opcode);
+}
+
+void Chip8::Do3XKK(ushort opcode)
+{
+	if (V[GetX(opcode)] == GetKK(opcode))
+		pc += 2;
+}
+
+void Chip8::Do4XKK(ushort opcode)
+{
+	if (V[GetX(opcode)] != GetKK(opcode))
+		pc += 2;
+}
+
+void Chip8::Do5XY0(ushort opcode)
+{
+	if (V[GetX(opcode)] == V[GetY(opcode)])
+		pc += 2;
+}
+
+void Chip8::Do6XKK(ushort opcode)
+{
+	V[GetX(opcode)] = GetKK(opcode);
+}
+
+void Chip8::Do7XKK(ushort opcode)
+{
+	V[GetX(opcode)] += GetKK(opcode);
+}
+
+void Chip8::Do8XY0(ushort opcode)
+{
+	V[GetX(opcode)] = V[GetY(opcode)];
+}
+
+void Chip8::Do8XY1(ushort opcode)
+{
+	V[GetX(opcode)] |= V[GetY(opcode)];
+}
+
+void Chip8::Do8XY2(ushort opcode)
+{
+	V[GetX(opcode)] &= V[GetY(opcode)];
+}
+
+void Chip8::Do8XY3(ushort opcode)
+{
+	V[GetX(opcode)] ^= V[GetY(opcode)];
+}
+
+void Chip8::Do8XY4(ushort opcode)
+{
+	ushort sum = V[GetX(opcode)] + V[GetY(opcode)];
+	V[GetX(opcode)] = static_cast<uchar>(sum);
+	V[0xF] = (sum > 255);
+}
+
+void Chip8::Do8XY5(ushort opcode)
+{
+	V[0xF] = (V[GetX(opcode)] > V[GetY(opcode)]);
+	V[GetX(opcode)] -= V[GetY(opcode)];
+}
+
+void Chip8::Do8XY6(ushort opcode)
+{
+	V[0xF] = V[GetX(opcode)] & 0x000F;
+	V[GetX(opcode)] >>= 1;
+}
+
+void Chip8::Do8XY7(ushort opcode)
+{
+	V[0xF] = (V[GetY(opcode)] > V[GetX(opcode)]);
+	V[GetX(opcode)] = V[GetY(opcode)] - V[GetX(opcode)];
+}
+
 Chip8::~Chip8()
 {
 }
--- Chip8VM/Chip8.h
+++ Chip8VM/Chip8.h
@@ -21,6 +21,22 @@ private:
 	uchar GetX(ushort opcode);
 	uchar GetY(ushort opcode);
 	uchar GetKK(ushort opcode);
+
+	void Do1NNN(ushort opcode);
+	void Do2NNN(ushort opcode);
+	void Do3XKK(ushort opcode);
+	void Do4XKK(ushort opcode);
+	void Do5XY0(ushort opcode);
+	void Do6XKK(ushort opcode);
+	void Do7XKK(ushort opcode);
+	void Do8XY0(ushort opcode);
+	void Do8XY1(ushort opcode);
+	void Do8XY2(ushort opcode);
+	void Do8XY3(ushort opcode);
+	void Do8XY4(ushort opcode);
+	void Do8XY5(ushort opcode);
+	void Do8XY6(ushort opcode);
+	void Do8XY7(ushort opcode);
 public:
 	Chip8();
 	~Chip8();
