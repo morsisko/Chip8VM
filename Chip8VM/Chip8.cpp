722ec189b97e02d124d9382ea1900cd3fab5add4
morsisko <morsisko@gmail.com>
2017-12-24 19:35:15 +0100

    ready table of opcodes

--- Chip8VM/Chip8.cpp
+++ Chip8VM/Chip8.cpp
@@ -45,6 +45,10 @@ void Chip8::Do1NNN(ushort opcode)
 	pc = GetNNN(opcode);
 }
 
+void Chip8::Do2NNN(ushort opcode)
+{
+}
+
 void Chip8::Do3XKK(ushort opcode)
 {
 	if (V[GetX(opcode)] == GetKK(opcode))
@@ -146,6 +150,18 @@ void Chip8::DoCXKK(ushort opcode)
 	V[GetX(opcode)] = random & GetKK(opcode);
 }
 
+void Chip8::DoDXYN(ushort opcode)
+{
+}
+
+void Chip8::DoEX9E(ushort opcode)
+{
+}
+
+void Chip8::DoEXA1(ushort opcode)
+{
+}
+
 void Chip8::DoFX07(ushort opcode)
 {
 	V[GetX(opcode)] = delay_timer;
@@ -171,9 +187,50 @@ void Chip8::DoFX1E(ushort opcode)
 	I += V[GetX(opcode)];
 }
 
+void Chip8::DoFX29(ushort opcode)
+{
+}
+
+void Chip8::DoFX33(ushort opcode)
+{
+}
+
+void Chip8::DoFX55(ushort opcode)
+{
+}
+
 void Chip8::InitializeFunctions()
 {
-	functions = { {&Chip8::Do1NNN, 0xF000, 0x1000} };
+	functions = { {&Chip8::Do1NNN, 0xF000, 0x1000},
+	{ &Chip8::Do2NNN, 0xF000, 0x2000 },
+	{ &Chip8::Do3XKK, 0xF000, 0x3000 }, 
+	{ &Chip8::Do4XKK, 0xF000, 0x4000 }, 
+	{ &Chip8::Do5XY0, 0xF00F, 0x5000 }, 
+	{ &Chip8::Do6XKK, 0xF000, 0x6000 }, 
+	{ &Chip8::Do7XKK, 0xF000, 0x7000 }, 
+	{ &Chip8::Do8XY1, 0xF00F, 0x8001 }, 
+	{ &Chip8::Do8XY2, 0xF00F, 0x8002 },
+	{ &Chip8::Do8XY3, 0xF00F, 0x8003 },
+	{ &Chip8::Do8XY4, 0xF00F, 0x8004 },
+	{ &Chip8::Do8XY5, 0xF00F, 0x8005 }, 
+	{ &Chip8::Do8XY6, 0xF00F, 0x8006 }, 
+	{ &Chip8::Do8XY7, 0xF00F, 0x8007 }, 
+	{ &Chip8::Do8XYE, 0xF00F, 0x800E }, 
+	{ &Chip8::Do9XY0, 0xF00F, 0x9000 }, 
+	{ &Chip8::DoANNN, 0xF000, 0xA000 },
+	{ &Chip8::DoBNNN, 0xF000, 0xB000 }, 
+	{ &Chip8::DoCXKK, 0xF000, 0xC000 }, 
+	{ &Chip8::DoDXYN, 0xF000, 0xD000 },
+	{ &Chip8::DoEX9E, 0xF0FF, 0xE09E },
+	{ &Chip8::DoEXA1, 0xF0FF, 0xE0A1 },
+	{ &Chip8::DoFX07, 0xF0FF, 0xF007 },
+	{ &Chip8::DoFX0A, 0xF0FF, 0xF00A },
+	{ &Chip8::DoFX15, 0xF0FF, 0xF015 }, 
+	{ &Chip8::DoFX18, 0xF0FF, 0xF018 },
+	{ &Chip8::DoFX1E, 0xF0FF, 0xF01E },
+	{ &Chip8::DoFX29, 0xF0FF, 0xF029 },
+	{ &Chip8::DoFX33, 0xF0FF, 0xF033 },
+	{ &Chip8::DoFX55, 0xF0FF, 0xF055 }, };
 }
 
 Chip8::~Chip8()
